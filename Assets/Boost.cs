ab24034d9c91d0e5bcaaaa6e9bf81f2d5cd46ebb
Daniel Tebbutt <moeruze.burning@gmail.com>
2016-10-02 14:00:54 +1300

    Balls

---
+++ Assets/Boost.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using BulletUnity;
+
+public class Boost : MonoBehaviour {
+
+    bool _boost = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_boost)
+        {
+            _boost = true;
+            var rigidBody = GetComponent<BRigidBody>();
+            rigidBody.AddImpulse(Vector3.back * 20);
+        }
+    }
+}
